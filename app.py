7daf2234b70766c79087cb7aeddbe39a53440f49
Torben0612 <24krahnstoevert@brooklinek12.org>
2023-06-03 08:30:39 -0400

    fixed some errors

--- app.py
+++ app.py
@@ -2,30 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/sims/physics/ac-circuits')
-def index():
+def ac_circuits():
     return render_template('ac-circuits.html')
 
 @app.route('/sims/physics/dc-circuits')
-def index():
+def dc_circuits():
     return render_template('dc-circuits.html')
 
 @app.route('/sims/physics/making-waves')
-def index():
+def making_waves():
     return render_template('making-waves.html')
 
 @app.route('/sims/physics/pendulum')
-def index():
+def pendulum():
     return render_template('pendulum.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host='0.0.0.0')
